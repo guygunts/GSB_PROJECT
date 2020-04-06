ce316e49612369b81852cdcdae5b412f420cd844
Detective Junior <b0855626503@gmail.com>
2020-04-06 19:20:44 +0700

    ลองเพิ่มหน้า config

--- setting.php
+++ setting.php
@@ -103,7 +103,31 @@ include_once 'service/service.php';
     <!-- /.login-box-body -->
 </div>
 </div>
-<div class="col-md-6"></div>
+<div class="col-md-6">
+    <div class="login-box">
+        <!-- /.login-logo -->
+        <div class="login-box-body">
+
+
+            <form action="" method="post" onsubmit="return false;" id="frmlogo">
+                <div class="form-group has-feedback">
+                    <label>Logo (Support Png Only)</label>
+                    <input type="file" class="form-control" placeholder="Sitename" name="logo">
+                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                </div>
+                <div class="row">
+                    <div class="col-xs-8">
+                    </div>
+                    <div class="col-xs-4">
+                        <button type="submit" class="btn btn-primary btn-block btn-flat">Save</button>
+                    </div>
+                </div>
+            </form>
+
+        </div>
+        <!-- /.login-box-body -->
+    </div>
+</div>
 
 <!-- jQuery 3 -->
 <script src="bower_components/jquery/dist/jquery.min.js"></script>
