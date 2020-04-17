563880d8c5fdc5f58b35e789cc94909a3bfeee13
Detective Junior <b0855626503@gmail.com>
2020-04-18 01:25:35 +0700

    fix deploy

--- module/conceptman/conceptman.script.js
+++ module/conceptman/conceptman.script.js
@@ -60,20 +60,19 @@ me.OpenPopup = function(){
 	if(cloneCount == 0){
 		maininput = maininput[0].outerHTML.replace(/dvvariation/g, 'dvvariation' + cloneCount);
 		$('div[id=variation]').append(maininput);
-		$('#dvvariation0 input[type="checkbox"]').iCheck({
-			checkboxClass: 'icheckbox_square-blue',
-			radioClass: 'iradio_square-blue',
-			labelHover: true,
-			increaseArea: '20%' // optional
-		});
-		$('#dvvariation0 input[type="checkbox"]').val(1);
-		$('#dvvariation0 input[type="checkbox"]').iCheck('check');
 	}else{
 		maininput = maininput[0].outerHTML.replace(/dvvariation/g, 'dvvariation' + cloneCount);
 		$('div[id^=dvvariation]').last().after(maininput);
-		$('#dvvariation'+cloneCount+' input[type="checkbox"]').val(1);
-		$('#dvvariation'+cloneCount+' input[type="checkbox"]').iCheck('check');
+
 	}
+	$('#dvvariation'+cloneCount+' input[type="checkbox"]').iCheck({
+		checkboxClass: 'icheckbox_square-blue',
+		radioClass: 'iradio_square-blue',
+		labelHover: true,
+		increaseArea: '20%' // optional
+	});
+	$('#dvvariation'+cloneCount+' input[type="checkbox"]').val(1);
+	$('#dvvariation'+cloneCount+' input[type="checkbox"]').iCheck('check');
 
 
 
