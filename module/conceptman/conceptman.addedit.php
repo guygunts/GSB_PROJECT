45d73a9fffa58244ad657d555af74fde16c86d8d
Detective Junior <b0855626503@gmail.com>
2020-04-17 23:14:46 +0700

    fix deploy

--- module/conceptman/conceptman.addedit.php
+++ module/conceptman/conceptman.addedit.php
@@ -20,34 +20,42 @@
                 </div>
                 <div class="form-group">
                     <label>
-                        <input type="checkbox" class="active" name="active" id="active" value="1"> &nbsp&nbsp&nbsp Active
+                        <input type="checkbox" class="active" name="active" id="active" value="1"> &nbsp&nbsp&nbsp
+                        Active
                     </label>
                 </div>
                 <div class="form-group">
-                    <button type="button" onclick="me.OpenPopup();" class="btn btn-info"><i class="fa fa-plus"></i> Add Variation</button>
+                    <button type="button" onclick="me.OpenPopup();" class="btn btn-info"><i class="fa fa-plus"></i> Add
+                        Variation
+                    </button>
                 </div>
 
 
             </div>
 
             <div class="col-md-12 sub" id="variation">
-                <div style="margin-left: 20px;margin-right: 20px;padding:20px;border: 1px dashed;" id="dvvariation" class="variationsub">
-                <div class="form-group">
-                    <label for="variation-concept_result" class="control-label">Concept Result <small style="color:red">*</small></label>
-                    <input id="variation-concept_result" name="variation-concept_result" type="text" maxlength="100"
-                           class="form-control empty" required>
-                </div>
-                <div class="form-group">
-                    <label for="variation-variation_text" class="control-label">Variation Name <small style="color:red">*</small></label>
-                    <input id="variation-variation_text"  name="variation-variation_text" type="text" maxlength="100"
-                           class="form-control variation-variation_text empty" required>
-                </div>
-                <div class="form-group">
-                    <label>
-                        <input type="checkbox" class="active" name="variation-active" id="variation-active" value="1"> &nbsp&nbsp&nbsp Active
-                    </label>
-                </div>
-                    <button type="button" class="btn btn-danger" style="float: right;"><i class="fa fa-minus"></i></button>
+                <div style="margin-left: 20px;margin-right: 20px;padding:20px;border: 1px dashed;" id="dvvariation"
+                     class="variationsub">
+                    <div class="form-group">
+                        <label for="variation-concept_result" class="control-label">Concept Result <small
+                                    style="color:red">*</small></label>
+                        <input id="variation-concept_result" name="variation-concept_result" type="text" maxlength="100"
+                               class="form-control empty" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="variation-variation_text" class="control-label">Variation Name <small
+                                    style="color:red">*</small></label>
+                        <input id="variation-variation_text" name="variation-variation_text" type="text" maxlength="100"
+                               class="form-control variation-variation_text empty" required>
+                    </div>
+                    <div class="form-group">
+                        <label>
+                            <input type="checkbox" class="active" name="variation-active" id="variation-active"
+                                   value="1"> &nbsp&nbsp&nbsp Active
+                        </label>
+                        <button type="button" class="btn btn-danger" style="float: right;"><i class="fa fa-minus"></i></button>
+
+                    </div>
                 </div>
             </div>
 
