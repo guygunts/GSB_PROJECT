6b14291f0856196dd21a24c63194b8174d0d9229
Detective Junior <b0855626503@gmail.com>
2020-04-20 23:34:55 +0700

    fix deploy

--- module/conceptman/conceptman.script.js
+++ module/conceptman/conceptman.script.js
@@ -304,8 +304,8 @@ me.LoadData = function(menu,page_id,page_size,readd=''){
 
 					$('#tbView tbody').on('click', 'td.details-control', function () {
 						var tr = $(this).closest('tr');
-						var row = me.table.row( tr );
-						var rowData = row.attr('data-name');
+						var row = me.table.row( tr ).nodes()[0];
+						var rowData = $(row).attr('data-name');
 						console.log(rowData);
 
 						if ( row.child.isShown() ) {
