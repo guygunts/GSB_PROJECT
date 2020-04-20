4c6542c6b3b93d2158696380c91f32d7350f2802
Detective Junior <b0855626503@gmail.com>
2020-04-20 19:26:49 +0700

    fix deploy

--- module/conceptman/conceptman.inc.php
+++ module/conceptman/conceptman.inc.php
@@ -56,7 +56,8 @@ function View(){
 
         foreach((array)$datas as $i => $item){
             $btn = '';
-
+            $item['DT_RowId'] = 'row_'.MD5($item[$columns[1]['data']]);
+            $datalist[$i]['DT_RowId'] = $item['DT_RowId'];
             $datalist[$i]['no'] = ($i+1);
 
             foreach((array)$columns as $v => $value){
