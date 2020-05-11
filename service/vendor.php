27c4fe1d770bdfe502b3c8423f1c9b25df59d62a
Detective Junior <b0855626503@gmail.com>
2020-05-12 00:08:18 +0700

    fix deploy

--- service/vendor.php
+++ service/vendor.php
@@ -3,15 +3,15 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use Laminas\Http\PhpEnvironment\Request;
 
-//header("Access-Control-Allow-Origin: * ");
-//header("Content-Type: application/json; charset=UTF-8");
-//header("Access-Control-Allow-Methods: POST,GET");
-//header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Origin: * ");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 
 $request = new Request();
 
-echo $request->getContent();
+echo $request->getQuery('mode');
 
 exit;
 $token = isset($_SESSION[OFFICE]['TOKEN']) ? $_SESSION[OFFICE]['TOKEN'] : '';
