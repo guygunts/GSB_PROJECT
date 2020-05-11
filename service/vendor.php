c2e0d85deec5d9453430da1042d9d05b9d0e84b6
Detective Junior <b0855626503@gmail.com>
2020-05-11 23:32:18 +0700

    fix deploy

--- service/vendor.php
+++ service/vendor.php
@@ -3,10 +3,10 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use Laminas\Http\Request;
 
-header("Access-Control-Allow-Origin: * ");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST,GET");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+//header("Access-Control-Allow-Origin: * ");
+//header("Content-Type: application/json; charset=UTF-8");
+//header("Access-Control-Allow-Methods: POST,GET");
+//header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 
 $request = new Request();
