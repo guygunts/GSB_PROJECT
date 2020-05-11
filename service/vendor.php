5014f0220663929693d01c8ca0b2180a215e3823
Detective Junior <b0855626503@gmail.com>
2020-05-12 01:10:01 +0700

    fix deploy

--- service/vendor.php
+++ service/vendor.php
@@ -4,16 +4,19 @@ require_once __DIR__.'/../vendor/autoload.php';
 //use Laminas\Http\PhpEnvironment\Request;
 //use Laminas\EventManager\EventInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 header("Access-Control-Allow-Origin: * ");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-$request = new Response();
+
+
+$request = Request::createFromGlobals();
 $request->getPathInfo();
 $request->overrideGlobals();
-echo $request->request->get('mode');
+echo $request->query->get('mode');
 exit;
 
 $request = new Request();
