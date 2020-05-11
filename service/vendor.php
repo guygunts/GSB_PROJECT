7a3efe452db6aaf10375142985516756ffde3148
Detective Junior <b0855626503@gmail.com>
2020-05-11 23:36:26 +0700

    fix deploy

--- service/vendor.php
+++ service/vendor.php
@@ -11,7 +11,7 @@ use Laminas\Http\Request;
 
 $request = new Request();
 
-$var = $request->getPost();
+$var = $request->getPost('username');
 print_r($var);
 exit;
 $token = isset($_SESSION[OFFICE]['TOKEN']) ? $_SESSION[OFFICE]['TOKEN'] : '';
