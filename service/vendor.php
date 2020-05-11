fd898cdb237700015d0f1d92a5c47672a1d669c8
Detective Junior <b0855626503@gmail.com>
2020-05-12 01:38:22 +0700

    fix deploy

--- service/vendor.php
+++ service/vendor.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 header("Access-Control-Allow-Origin: * ");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST,GET");
+//header("Access-Control-Allow-Methods: POST,GET");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 
