e9b5ccfac308f2625bd69f9280e8980943e982ad
Detective Junior <b0855626503@gmail.com>
2020-05-12 13:08:24 +0700

    fix deploy

--- api.inc.php
+++ api.inc.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Controller;
 
 require_once "service/service.php";
 require_once "service/vendor.php";
--- service/vendor.php
+++ service/vendor.php
@@ -1,11 +1,9 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
 
-use Laminas\Http\PhpEnvironment\Request as Request;
-//use Laminas\EventManager\EventInterface;
-//use App\Http\SpecialRequest;
-//use Symfony\Component\HttpFoundation\Request;
-//use Symfony\Component\HttpFoundation\Response;
+use \Laminas\Http\PhpEnvironment\Request as Requests;
+use Illuminate\Http\Request;
+
 
 header("Access-Control-Allow-Origin: * ");
 header("Content-Type: application/json; charset=UTF-8");
@@ -13,6 +11,6 @@ header("Access-Control-Allow-Methods: POST,GET");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 
-$request = new Request();
+$request = new Requests();
 
 $token = isset($_SESSION[OFFICE]['TOKEN']) ? $_SESSION[OFFICE]['TOKEN'] : '';
