f60819ca2cf7d852779e0f94b92864d297f73218
Detective Junior <b0855626503@gmail.com>
2020-05-12 13:20:38 +0700

    fix deploy

--- api.inc.php
+++ api.inc.php
@@ -8,7 +8,7 @@ class Api
 {
     protected $request;
 
-    public static function Login(HttpRequest $request)
+    public static function Login(\Laminas\Http\Request $request)
     {
 
         parse_str($request->getPost()->toString(), $data);
