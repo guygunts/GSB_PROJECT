94d6694034aa3abdc49a3c8416f963a6c4ba796a
Detective Junior <b0855626503@gmail.com>
2020-05-13 02:14:03 +0700

    fix deploy

--- service/define.php
+++ service/define.php
@@ -33,45 +33,45 @@ define('DEL', '3');
 define('PRINT', '4');
 
 /* :: http://creation.ctdserver.com :: */
-$c_site = "SITE.txt";
-$c_site_open = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/config/'.$c_site);
-if ($c_site_open) {
-    $c_site_file = $c_site_open;
-}else{
-    $c_site_file = 'TESTER';
-}
+//$c_site = "SITE.txt";
+//$c_site_open = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/config/'.$c_site);
+//if ($c_site_open) {
+//    $c_site_file = $c_site_open;
+//}else{
+//    $c_site_file = 'TESTER';
+//}
+//
+//$c_url = "URL.txt";
+//$c_url_open = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/config/'.$c_url);
+//if ($c_url_open) {
+//    $c_file = $c_url_open;
+//}else{
+//    $c_file = 'https://gsb.devtool77.com';
+//}
+//
+//$c_api = "URL_API.txt";
+//$c_urlapi_open = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/config/'.$c_api);
+//if ($c_urlapi_open) {
+//    $c_api_file = $c_urlapi_open;
+//}else{
+//    $c_api_file = 'https://wso2ei.snapz.mobi';
+//}
+//
+//$c_color = "COLOR.txt";
+//$c_color_open = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/config/'.$c_color);
+//if ($c_color_open) {
+//    $c_color_file = $c_color_open;
+//}else{
+//    $c_color_file = '#ec068d';
+//}
 
-$c_url = "URL.txt";
-$c_url_open = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/config/'.$c_url);
-if ($c_url_open) {
-    $c_file = $c_url_open;
-}else{
-    $c_file = 'https://gsb.devtool77.com';
-}
-
-$c_api = "URL_API.txt";
-$c_urlapi_open = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/config/'.$c_api);
-if ($c_urlapi_open) {
-    $c_api_file = $c_urlapi_open;
-}else{
-    $c_api_file = 'https://wso2ei.snapz.mobi';
-}
-
-$c_color = "COLOR.txt";
-$c_color_open = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/config/'.$c_color);
-if ($c_color_open) {
-    $c_color_file = $c_color_open;
-}else{
-    $c_color_file = '#ec068d';
-}
 
 
-
-define("COLOR", $c_color_file);
-define("SITE", $c_site_file);
-define("URL", $c_file);
-define("BASEURL", $c_file);
-define("URL_API", $c_api_file);
+define("COLOR", '#ec068d');
+define("SITE", 'GSB');
+define("URL", 'https://gsb.devtool77.com');
+define("BASEURL", 'https://gsb.devtool77.com');
+define("URL_API", 'https://wso2ei.snapz.mobi');
 //define("URL", "http://localhost");
 //define("BASEURL", "http://localhost");
 //define("URL_API", "http://localhost/api");
--- service/vendor.php
+++ service/vendor.php
@@ -26,7 +26,7 @@ $x = new MyRequest();
 
 
 
-header("Access-Control-Allow-Origin: * ");
+header("Access-Control-Allow-Origin: ".URL." ");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST,GET");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
