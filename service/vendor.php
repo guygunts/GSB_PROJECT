578ed5735d79594d440193e74517f26a2b32ba4a
Detective Junior <b0855626503@gmail.com>
2020-05-12 19:32:53 +0700

    fix deploy

--- service/vendor.php
+++ service/vendor.php
@@ -1,8 +1,7 @@
 <?php
-require_once __DIR__ . '../../vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 use Laminas\Http\PhpEnvironment\Request as MainRequest;
-
 $request = new MainRequest();
 
 
@@ -17,16 +16,21 @@ class MyRequest extends MainRequest implements Request
 
     public function __construct()
     {
-        $this->request = new MainRequest();
+        $request = new MainRequest();
+        self::$request = $request;
+        return self::$request;
     }
 
 }
+
 
 
 header("Access-Control-Allow-Origin: * ");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST,GET");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+
 
 
 $token = isset($_SESSION[OFFICE]['TOKEN']) ? $_SESSION[OFFICE]['TOKEN'] : '';
