2322e6b8aec5764c047f6594785a377cff932df6
Detective Junior <b0855626503@gmail.com>
2020-05-21 14:41:08 +0700

    fix vendor

--- service/service.php
+++ service/service.php
@@ -9,6 +9,8 @@ header("Set-Cookie: id=a3fWa; Expires=Wed, 21 Oct 2015 07:28:00 GMT; Secure; Htt
 header("X-XSS-Protection: 1; mode=block");
 header("Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' cdn.jsdelivr.net d3js.org canvasjs.com cdn.datatables.net cdnjs.cloudflare.com; style-src 'self' 'unsafe-inline' cdn.datatables.net cdnjs.cloudflare.com fonts.googleapis.com; font-src 'self' fonts.gstatic.com; img-src * data:");
 header("X-Content-Type-Options: nosniff");
+header("Referrer-Policy: same-origin");
+header("Feature-Policy: vibrate 'none'");
 header("Strict-Transport-Security: max-age=631138519; includeSubDomains");
 header("Content-Type: text/html; charset=UTF-8");
 header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
