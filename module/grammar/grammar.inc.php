c4d3d8488c577fde01caadd10f851c33fcbf0952
Detective Junior <b0855626503@gmail.com>
2020-05-25 02:53:53 +0700

    fix module

--- module/grammar/grammar.inc.php
+++ module/grammar/grammar.inc.php
@@ -279,6 +279,11 @@ function LoadCbo(Request $request)
         foreach ((array)$datas as $i => $item) {
             $datalist[$i]['text'] =$item[$data['name']];
             $datalist[$i]['id'] =$item[$data['code']];
+            $datalist[$i]['icon'] ='glyphicon glyphicon-stop';
+            $datalist[$i]['selectedIcon'] ='glyphicon glyphicon-stop';
+            $datalist[$i]['selectable'] = true;
+            $datalist[$i]['state'] = ['selected',true];
+            $datalist[$i]['nodes'] = [];
         }
 
 
@@ -322,11 +327,7 @@ function LoadCboSub(Request $request)
         foreach ((array)$datas as $i => $item) {
             $datalist[$i]['text'] =$item['category_name'];
             $datalist[$i]['id'] =$item['category_id'];
-            $datalist[$i]['icon'] ='glyphicon glyphicon-stop';
-            $datalist[$i]['selectedIcon'] ='glyphicon glyphicon-stop';
-            $datalist[$i]['selectable'] = true;
-            $datalist[$i]['state'] = ['selected',true];
-            $datalist[$i]['nodes'] = [];
+
         }
 
 
