9a51ddeae2dcd943af628d2a0537525168bd5232
Detective Junior <b0855626503@gmail.com>
2020-05-25 00:18:03 +0700

    fix vendor

--- service/func.php
+++ service/func.php
@@ -1551,9 +1551,9 @@ function curlpost($url, $params)
     $response = curl_exec($ch);
     curl_close($ch);
     if ($response) {
-        $response = array_filter(json_decode($response, true));
+//        $response = array_filter(json_decode($response, true));
     } else {
-        $response = array();
+//        $response = array();
     }
 
 
