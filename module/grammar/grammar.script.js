96f6fedc818754e397d01d58d1c68aa55712dd40
Detective Junior <b0855626503@gmail.com>
2020-05-25 02:49:50 +0700

    fix module

--- module/grammar/grammar.script.js
+++ module/grammar/grammar.script.js
@@ -54,7 +54,7 @@ me.LoadCboSub = function(val,menu,code,nodeid) {
 
 			switch (data.success) {
 				case "COMPLETE":
-					$('#'+val).treeview('expandNode', [ nodeid , data.item ]);
+					$('#'+val).treeview('revealNode', [ nodeid , data.item ]);
 		break;
 		default:
 			alertify.alert(data.msg);
