fe480e4f663eff7efb3b558bc7ce3854b2c055e8
Detective Junior <b0855626503@gmail.com>
2020-05-25 03:58:32 +0700

    fix module

--- module/grammar/grammar.script.js
+++ module/grammar/grammar.script.js
@@ -29,6 +29,8 @@ me.LoadCbo = function(val,menu,code,name) {
 						levels: 1,
 						data: data.item,
 						onNodeSelected: function(event, data) {
+							console.log(event);
+							console.log(data);
 							me.LoadCboSub('tree','getsubcategory',data.id,data.index);
 						}
 
