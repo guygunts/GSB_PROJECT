aab42fce2e1b0ea8909144cd0696253e4fc4a08c
Detective Junior <b0855626503@gmail.com>
2020-05-25 03:02:32 +0700

    fix module

--- module/grammar/grammar.inc.php
+++ module/grammar/grammar.inc.php
@@ -279,7 +279,6 @@ function LoadCbo(Request $request)
         foreach ((array)$datas as $i => $item) {
             $datalist[$i]['text'] =$item[$data['name']];
             $datalist[$i]['id'] =$item[$data['code']];
-            $datalist[$i]['selectable'] = true;
             $datalist[$i]['nodes'] = [];
         }
 
