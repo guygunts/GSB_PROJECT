8ae18d1b072cf3a1a0751f45586ac5daa13c3086
Detective Junior <b0855626503@gmail.com>
2020-05-25 03:49:53 +0700

    fix module

--- module/grammar/grammar.script.js
+++ module/grammar/grammar.script.js
@@ -26,7 +26,7 @@ me.LoadCbo = function(val,menu,code,name) {
 			switch (data.success) {
 				case "COMPLETE":
 					$('#'+val).treeview({
-						level: 1,
+						levels: 1,
 						data: data.item,
 						onNodeSelected: function(event, data) {
 							console.log(event);
