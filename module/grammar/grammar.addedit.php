22b4b0ba89ec4b20533a4ac18395d651b20f49a2
Detective Junior <b0855626503@gmail.com>
2020-05-25 18:39:53 +0700

    fix module

--- module/grammar/grammar.addedit.php
+++ module/grammar/grammar.addedit.php
@@ -1,30 +1,68 @@
 <form id="frm_addedit" method="post" onsubmit="return false;">
     <button type="submit" style="display: none" id="btnsubmit"></button>
     <input type="hidden" name="code" id="code">
-    <input type="hidden" name="function_id" id="function_id">
-    <input type="hidden" name="menu_action" id="menu_action">
+    <input type="hidden" name="category_id">
+    <input type="hidden" name="menu_action">
     <div class="box box-danger">
         <div class="box-body">
             <div class="col-md-12">
                 <div class="form-group">
-                    <label for="function_name" class="control-label">function Name</label>
-                    <input id="function_name" name="function_name" type="text" required="required" maxlength="100"
+                    <label for="user_question" class="control-label">User Question</label>
+                    <input id="user_question" name="user_question" type="text" required="required" maxlength="100"
                            class="form-control">
                 </div>
                 <div class="form-group">
-                    <label for="function_desc" class="control-label">function Description</label>
-                    <input id="function_desc" name="function_desc" type="text" required="required" maxlength="255"
+                    <label for="intent_tag" class="control-label">Intent Tag</label>
+                    <input id="intent_tag" name="intent_tag" type="text" required="required" maxlength="255"
                            class="form-control">
                 </div>
                 <div class="form-group">
                     <label>
-                        <input type="checkbox" class="active" name="function_active" id="function_active" value="1"> &nbsp&nbsp&nbsp Active
+                        <input type="checkbox" class="active" name="active" id="active" value="1"> &nbsp&nbsp&nbsp
+                        Active
                     </label>
                 </div>
+                div class="form-group">
+                <button type="button" onclick="me.OpenPopup();" class="btn btn-info btn_add btn-xs" id="addvariation"><i
+                            class="fa fa-plus"></i> Add
+                    Sub
+                </button>
+            </div>
 
+            <div class="col-md-12 sub" id="variation">
+                <div style="margin-left: 20px;margin-right: 20px;padding:20px;border: 1px dashed;" id="dvvariation"
+                     class="variationsub row">
+                    <div class="col-md-12">
+                        <div class="form-group col-md-6">
+<!--                            <input name="variation[zero][concept_variation_id]" id="mconcept_variation_id" type="hidden">-->
+                            <label for="variation-concept_result" class="control-label">Intent TAG <small
+                                        style="color:red">*</small></label>
+                            <input id="mvariation-concept_result" name="variation[zero][subintent_tag]" type="text" maxlength="100"
+                                   class="form-control empty" required>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="variation-type" class="control-label">Type <small
+                                        style="color:red">*</small></label>
+                            <select id="mvariation-type" name="variation[zero][type]" class="form-control variation-type empty" required>
+                                <option value="0">Robust</option>
+                                <option value="1" selected>Static</option>
+                            </select>
+                        </div>
+                        <div class="form-group col-md-12">
+                            <label>
+                                <input type="checkbox" class="active" name="variation[zero][active]" id="mvariation-active"
+                                       value="1"> &nbsp&nbsp&nbsp Active
+                            </label>
+                            <button type="button" class="btn btn-danger btn-xs btn_add" style="float: right;" data-code="dvvariation" onclick="me.RemoveSub(this)"><i class="fa fa-trash-o"></i></button>
+
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
+
     </div>
+
 
 </form>
