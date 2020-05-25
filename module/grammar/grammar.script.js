5a7f816a0f76c1ceacf13e7ceaf663c1af390e14
Detective Junior <b0855626503@gmail.com>
2020-05-25 20:10:30 +0700

    fix module

--- module/grammar/grammar.script.js
+++ module/grammar/grammar.script.js
@@ -259,6 +259,53 @@ me.format = function (rowData) {
     return '<div class="col-md-10" style="margin: 0 auto;float: none;padding: 10px;"><table id="' + rowData.name.replace(' ', '-') + '" class="table table-yellow table-bordered table-striped table-condensed dataTable" style="width: 100%;"></table></div>';
 }
 
+me.Add = function () {
+    $('#btnsubmit').click(function (e) {
+        e.stopPropagation();
+        // if($('#variation-variation_text').attr('required') == 'required'){
+        //     if(!$('#variation-variation_text').val()){
+        //         $('#variation-variation_text').tagsinput('focus');
+        //         return false;
+        //     }
+        // }
+        $('form#frm_addedit').submit(function () {
+            var form = $(this);
+            $('.modal').modal('hide');
+            alertify.confirm("Do you want Add.",
+                function () {
+                    $.ajax({
+                        url: me.url + '-Add',
+                        type: 'POST',
+                        dataType: 'json',
+                        cache: false,
+                        data: form.serialize({
+                            checkboxesAsBools: true
+                        }),
+                        success: function (data) {
+                            switch (data.success) {
+                                case 'COMPLETE':
+                                    $('.modal').modal('hide');
+                                    alertify.success(data.msg);
+                                    // $('#btnsearchsubmit').click();
+                                    // me.table.clear().draw();
+                                    me.LoadData(me.action.menu,me.code, 1, 30, 1);
+                                    break;
+                                default:
+                                    alertify.error(data.msg);
+                                    break;
+                            }
+                        }
+                    });
+                },
+                function () {
+                    alertify.error('Cancel Add');
+                });
+
+        });
+
+    }).click();
+};
+
 me.AddSub = function () {
     $('#btnsubmitadd').click(function (e) {
         e.stopPropagation();
