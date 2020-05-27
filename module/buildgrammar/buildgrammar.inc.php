19da1e36e3efb283479b7e5fe7bc9c7b52fc4edc
Detective Junior <b0855626503@gmail.com>
2020-05-27 13:52:05 +0700

    fix module

--- module/buildgrammar/buildgrammar.inc.php
+++ module/buildgrammar/buildgrammar.inc.php
@@ -177,7 +177,7 @@ function Add(Request $request)
 
     parse_str($request->getPost()->toString(), $data);
 
-    $file = $request->getFiles();
+    $file = $request->getFiles()->get('name');
     PrintR($file);
     exit;
 //    $file = $_FILES['file'];
