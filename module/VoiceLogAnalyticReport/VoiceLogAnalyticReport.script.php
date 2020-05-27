2add083e72f592e9898d77c55ecd4ddb8bbc3b1e
Detective Junior <b0855626503@gmail.com>
2020-05-27 23:40:15 +0700

    fix module

--- module/VoiceLogAnalyticReport/VoiceLogAnalyticReport.script.php
+++ module/VoiceLogAnalyticReport/VoiceLogAnalyticReport.script.php
@@ -1,8 +1,6 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.3.0/jszip.min.js"></script>
 <script src="bower_components/pdfmake/build/pdfmake.js"></script>
 <script src="bower_components/pdfmake/build/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.print.min.js"></script>
 <script>
     pdfMake.fonts = {
         THSarabunNew: {
--- module/deploygrammar/deploygrammar.script.php
+++ module/deploygrammar/deploygrammar.script.php
@@ -1,11 +1,7 @@
 <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.3.0/jszip.min.js"></script>
 <script src="bower_components/pdfmake/build/pdfmake.js"></script>
 <script src="bower_components/pdfmake/build/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.print.min.js"></script>
 <script>
     pdfMake.fonts = {
         THSarabunNew: {
