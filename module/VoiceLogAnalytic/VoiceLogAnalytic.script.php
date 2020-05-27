e639ccbb3386877d8ee75f5c0681aae8d7222843
Detective Junior <b0855626503@gmail.com>
2020-05-27 19:27:34 +0700

    fix module

--- module/VoiceLogAnalytic/VoiceLogAnalytic.script.php
+++ module/VoiceLogAnalytic/VoiceLogAnalytic.script.php
@@ -2,7 +2,6 @@
 <script src="bower_components/pdfmake/build/pdfmake.js"></script>
 <script src="bower_components/pdfmake/build/vfs_fonts.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.bootstrap.min.js"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.colVis.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.html5.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.print.min.js"></script>
 <script>
