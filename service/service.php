2047e3588f9bd154c2d603a24f65d76e47d71f6d
Detective Junior <b0855626503@gmail.com>
2020-05-27 10:22:42 +0700

    fix module

--- service/service.php
+++ service/service.php
@@ -1,7 +1,8 @@
 <?php
+require_once "define.php";
 ini_set('session.cookie_httponly', 1);
 session_start();
-require_once "define.php";
+
 require_once "func.php";
 
 header("Access-Control-Allow-Credentials: true");
