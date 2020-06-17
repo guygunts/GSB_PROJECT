6fb1d22b0ae157ecca79d096bc1b8aaad09fd427
Detective Junior <b0855626503@gmail.com>
2020-06-17 17:36:37 +0700

    แก้ไข ตาม api

--- module/roleman/roleman.inc.php
+++ module/roleman/roleman.inc.php
@@ -26,9 +26,9 @@ function View(Request $request)
     $response = curlposttoken($url, $params, $token);
 
 
-    if ($response['code'] == 200) {
-        $columnslist = $response['result']['header'];
-        $datas = $response['result']['data'];
+    if ($response['result'][0]['code'] == 200) {
+        $columnslist = $response['columnsname'];
+        $datas = $response['recs'];
 
         $column[0]['className'] = 'text-center';
         $column[0]['title'] = 'No';
