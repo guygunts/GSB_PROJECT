50e1552d88722eef265a0c40f16031f98de19105
Detective Junior <b0855626503@gmail.com>
2020-07-13 20:45:39 +0700

    แก้ไข ตาม api

--- module/VoiceLogAnalyticReport/excel.php
+++ module/VoiceLogAnalyticReport/excel.php
@@ -227,7 +227,7 @@ $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load('template.xlsx');
 //$spreadsheet = new Spreadsheet();
 $main = $spreadsheet->setActiveSheetIndex(0);
 //$main->getColumnDimension('A')->setWidth(12);
-$main->fromArray($datalist, NULL, 'A24');
+$main->fromArray($datalist, 0, 'A24');
 //$main->setCellValue('A24', $datalist[0]['Intent']);
 $main->setCellValue('G2', $customer);
 $main->setCellValue('G4', $project);
